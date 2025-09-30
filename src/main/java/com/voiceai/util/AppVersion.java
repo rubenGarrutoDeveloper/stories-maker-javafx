b2a7a088b382ee6ea4cd4baba3b4858f26855c81
rubenGarrutoDeveloper <ruben.garruto.developer@gmail.com>
2025-09-30 20:48:47 +0200

    🚀 Bump version to 1.1.0

--- src/main/java/com/voiceai/util/AppVersion.java
+++ src/main/java/com/voiceai/util/AppVersion.java
@@ -9,7 +9,7 @@ public class AppVersion {
      * Current application version
      * Format: MAJOR.MINOR.PATCH
      */
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     /**
      * Application build date (can be updated during build process)
